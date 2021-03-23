a974b984dbc4022215944b3ffa5fac7bb4210924
loveCoding01 <ishan.karmakar24@gmail.com>
2021-03-23 13:44:22 -0500

    Remove ban, unban, kick methods

--- discord_methods_general.py
+++ discord_methods_general.py
@@ -18,25 +18,6 @@ class General(commands.Cog):
         nick = ' '.join(nick)
         await ctx.me.edit(nick=nick)
 
-    @commands.command(brief='Bans member', description='Syntax: !ban @<member name> <reason>')
-    @has_permissions(ban_members=True)
-    async def ban(self, ctx, member: discord.User, *reason) -> None:
-        reason = ' '.join(reason)
-        await ctx.guild.ban(member, reason=reason)
-        await ctx.reply('User has been banned')
-
-    @commands.command(brief='Unbans member', description='Syntax: !unban @<member name> <reason>')
-    @has_permissions(ban_members=True)
-    async def unban(self, ctx, member: discord.User) -> None:
-        await ctx.guild.unban(member)
-        await ctx.reply('User has been unbanned')
-
-    @commands.command(brief='Kicks member', description='Syntax: !kick @<member name>')
-    @has_permissions(kick_members=True)
-    async def kick(self, ctx, member: discord.Member) -> None:
-        await member.kick()
-        await ctx.reply('User has been kicked')
-
     @commands.command(brief='Mute someone', description='Mute will mute a member. Just pass in the member with an @ sign and 1 or 0 for muted or not')
     @has_permissions(ban_members=True)
     async def mute(self, ctx, member: Member, value: bool):
@@ -47,9 +28,6 @@ class General(commands.Cog):
         await ctx.send('{member} has been {status}'.format(member=member.display_name, status='muted' if value else 'unmuted'))
 
     @mute.error
-    @ban.error
-    @kick.error
-    @unban.error
     async def permissions_error(self, ctx, error):
         if isinstance(error, MissingPermissions):
             await ctx.send('You don\'t have the correct permissions')
