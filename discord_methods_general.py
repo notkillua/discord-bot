e1692d557127b9c58e05ebd1e97f8c34efa2386e
loveCoding01 <ishan.karmakar24@gmail.com>
2021-03-25 10:17:47 -0500

    Remove unmute and mute commands

--- discord_methods_general.py
+++ discord_methods_general.py
@@ -18,29 +18,6 @@ class General(commands.Cog):
         nick = ' '.join(nick)
         await ctx.me.edit(nick=nick)
 
-    @commands.command(brief='Mute someone', description='Mute will mute a member. Just pass in the member with an @ sign')
-    @has_permissions(mute_members=True)
-    async def mute(self, ctx, member: Member):
-        try:
-            await member.edit(mute=True)
-        except discord.errors.HTTPException:
-            return await ctx.reply('User must be connected to a voice channel')
-        await ctx.send(f'{member.display_name} has been muted')
-
-    @commands.command(brief='Unmute someone', description='Unmute will unmute a member. Just pass in the member with an @ sign')
-    @has_permissions(mute_members=True)
-    async def mute(self, ctx, member: Member):
-        try:
-            await member.edit(mute=False)
-        except discord.errors.HTTPException:
-            return await ctx.reply('User must be connected to a voice channel')
-        await ctx.send(f'{member.display_name} has been unmuted')
-
-    @mute.error
-    async def permissions_error(self, ctx, error):
-        if isinstance(error, MissingPermissions):
-            await ctx.send('You don\'t have the correct permissions')
-
 
 def setup(bot):
     bot.add_cog(General(bot))
