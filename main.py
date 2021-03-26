b478c99d2a39ea51cbb968e7d09471ca4f5a1754
notkillua <ryanchazari@gmail.com>
2021-03-26 16:37:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,20 +11,6 @@ db = client['discordbot_data']
 TOKEN = env['DEV_TOKEN'] if sys.argv[1] == 'dev' else env['PROD_TOKEN']
 # Get prefixes collection
 prefixes = db['prefixes']
-# All the parts of the embed
-description = '''
-Musical is a music bot that can play music in voice channels.
-Musical is very similar to another discord bot called Rhythm.
-Both bots have the same purpose of playing music, and have queues and playlists.
-'''
-important_commands = '''
-The default command prefix is !, but you can change it with !prefix <new prefix>.
-Some important commands are !queue (to show queue), !add <search term or youtube url> (add song to queue),
-!remove <index of song> (remove song from queue), and !mv <begin> <end> (move songs around in queue).
-For all the commands, run the command !help
-'''
-embed = discord.Embed(title='Musical Discord Bot', description=description)
-embed.add_field(name='Import Commands', value=important_commands, inline=False)
 # Print out which type of bot is being run
 if sys.argv[1] == 'dev':
     print('Development discord bot started')
@@ -64,10 +50,6 @@ async def on_guild_join(guild: discord.Guild):
         await myBot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'{server_count} server'))
     else:
         await myBot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'{server_count} servers'))
-    for channel in guild.text_channels:
-        if channel.permissions_for(guild.me):
-            await channel.send(embed=embed)
-            break
 
 # Remove prefix when removed
 
